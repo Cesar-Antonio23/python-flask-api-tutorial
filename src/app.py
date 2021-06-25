dcd0cf93979a57d5a00123f5035f109bed8661dc
Cesar-Antonio23 <ces.acevedo.23@gmail.com>
2021-06-25 17:07:50 +0000

    Finishing

--- src/app.py
+++ src/app.py
@@ -6,9 +6,23 @@ todos = [
     { "label": "My first task", "done": False}
 ]
 
-@app.route('/todos', methods=["GET"])
+@app.route("/todos", methods=["GET"])
 def hello_world():
     json_todos = jsonify(todos)
     return json_todos
 
-    
+@app.route("/todos", methods=["POST"])
+def add_new_todo():
+    request_body = json.loads(request.data)
+    todos.append(request_body)
+    json_todos = jsonify(todos)
+    return json_todos
+
+@app.route("/todos/<int:position>", methods = ["DELETE"])
+def delete_todo(position):
+    todos.pop(position)
+    json_todos = jsonify(todos)
+    return json_todos    
+     
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)  
